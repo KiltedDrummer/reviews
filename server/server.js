7158a0bf03decd6db9cd7611d0626b80784c3e1c
Lisette <palomahamilton@gmail.com>
2018-09-18 20:25:20 -0700

    added CORS header

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const app = express();
 // serve up static files inside public
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 app.get('/reviews/:id', bodyParser.json(), (req, res) => {
   //depending on this id the index.js will do a different query
   var idNumber = parseInt(req.params.id)
